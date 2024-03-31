1939c8cdb335cf0f70a89709dd025fa43a784eaf
by5747 <kimbomi@KimBomi.local>
2024-03-31 23:17:17 +0900

    update 22

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-// Google Maps API를 사용하여 지도 표시
-function initMap() {
-    var myLatLng = {lat: 37.5665, lng: 126.9780}; // 예시 위치 (서울)
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: myLatLng
-    });
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: '결혼식장'
-    });
-}
-
-function openModal(imageSrc) {
-    document.getElementById("myModal").style.display = "block";
-    document.getElementById("modalImg").src = imageSrc;
-}
-
-// 모달 닫기
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
-
-
 function copyToClipboard(val) {
     var el = document.createElement('textarea');
     el.value = val;
